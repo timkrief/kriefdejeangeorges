397daf66d505f819c665527067e50ff2b082ff51
sebastien_Dejean-Georges <seb.dg@neuf.fr>
2018-11-08 20:30:56 +0100

    Modification pdf report

---
+++ src/client/render/GMap.cpp
@@ -0,0 +1,19 @@
+#include "GMap.h"
+#include <SFML/Graphics.hpp>
+
+namespace render {
+
+  /// class GMap - 
+  class GMap {
+    // Operations
+  GMap::Gmap(){
+    
+  }
+
+  
+  //void draw (state::GState state);
+    
+  
+  //};
+
+};

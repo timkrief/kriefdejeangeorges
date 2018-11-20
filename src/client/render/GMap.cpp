9de47e0edded05c9b459fadf61fb47bc3a58be48
sebastien_Dejean-Georges <seb.dg@neuf.fr>
2018-11-20 17:06:23 +0100

    render working at 90%

--- src/client/render/GMap.cpp
+++ src/client/render/GMap.cpp
@@ -21,13 +21,7 @@ namespace render {
     for(unsigned int x = 0; x < map->getSize().x; x++){
       for(unsigned int y = 0; y < map->getSize().y; y++){
         
-
-        unsigned int tx = (map->getData(sf::Vector2u(x,y))  %   tileset.getColumns()-1)* 16; 
-        unsigned int ty = (int)(map->getData(sf::Vector2u(x,y)) /   tileset.getColumns())* 16;
-        
-
-
-        sf::IntRect rectangle(tx,ty,16,16);
+        sf::IntRect rectangle = tileset.getTileIntRect(map->getData(sf::Vector2u(x,y)));
         sf::Sprite sprite(*(TextureManager::getTexture("mapTexture")), rectangle);
         sprite.setPosition(x*16,y*16);
         target.draw(sprite);
